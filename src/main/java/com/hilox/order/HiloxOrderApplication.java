4a1ce273f671454339ebf2fb27e331131ba36231
zhaohaihao <hanxq@vip.qq.com>
2018-12-06 17:51:38 +0800

    [add]初始提交

---
+++ src/main/java/com/hilox/order/HiloxOrderApplication.java
@@ -0,0 +1,12 @@
+package com.hilox.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiloxOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HiloxOrderApplication.class, args);
+	}
+}
---
+++ src/test/java/com/hilox/order/HiloxOrderApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hilox.order;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HiloxOrderApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
