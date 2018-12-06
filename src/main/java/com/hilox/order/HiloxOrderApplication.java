9c2a8e5077c9f29d926414a166c1cec077a8704f
zhaohaihao <hanxq@vip.qq.com>
2018-12-06 19:09:01 +0800

    [add]日志

--- src/main/java/com/hilox/order/HiloxOrderApplication.java
+++ src/main/java/com/hilox/order/HiloxOrderApplication.java
@@ -3,6 +3,10 @@ package com.hilox.order;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 启动项
+ * Created by Hilox on 2018/12/6 0006.
+ */
 @SpringBootApplication
 public class HiloxOrderApplication {
 
--- src/test/java/com/hilox/order/HiloxOrderApplicationTests.java
+++ src/test/java/com/hilox/order/HiloxOrderApplicationTests.java
@@ -1,16 +1,25 @@
 package com.hilox.order;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+/**
+ * 测试类
+ * Created by Hilox on 2018/12/6 0006.
+ */
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Slf4j
 public class HiloxOrderApplicationTests {
 
 	@Test
-	public void contextLoads() {
+	public void testLogger() {
+		log.info("info");
+		log.error("error");
+		log.debug("debug");
 	}
 
 }
