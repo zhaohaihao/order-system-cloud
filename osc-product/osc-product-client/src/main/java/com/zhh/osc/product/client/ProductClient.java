9f3f8cb15e412501e660ec3e15094d643a59e38b
zhaohaihao <zhh2933@gmail.com>
2020-04-27 11:04:08 +0800

    feat: 版本升级

--- osc-product/osc-product-client/src/main/java/com/zhh/osc/product/client/ProductClient.java
+++ osc-product/osc-product-client/src/main/java/com/zhh/osc/product/client/ProductClient.java
@@ -2,7 +2,7 @@ package com.zhh.osc.product.client;
 
 import com.zhh.osc.product.common.DecreaseStockInput;
 import com.zhh.osc.product.common.ProductInfoOutput;
-import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
